0576971419332be8e08926209c16b1fcbe2c67aa
sthanasis <sthanasis1994@gmail.com>
2024-11-22 17:29:57 +0200

    internalization setup

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import Navbar from '@/features/navbar/components/Navbar';
-import './globals.css';
+import '../globals.css';
 
 const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
+  src: '../fonts/GeistVF.woff',
   variable: '--font-geist-sans',
   weight: '100 900',
 });
 const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
+  src: '../fonts/GeistMonoVF.woff',
   variable: '--font-geist-mono',
   weight: '100 900',
 });
@@ -19,13 +19,20 @@ export const metadata: Metadata = {
   description: 'An online shop',
 };
 
-export default function RootLayout({
+export async function generateStaticParams() {
+  return [{ lang: 'en-US' }, { lang: 'el-GR' }];
+}
+
+export default async function RootLayout({
   children,
+  params,
 }: Readonly<{
   children: React.ReactNode;
+  params: { lang: string };
 }>) {
+  const { lang } = await params;
   return (
-    <html lang="en">
+    <html lang={lang}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} overflow-auto w-screen pt-16 h-screen  antialiased font-[family-name:var(--font-geist-sans)]`}
       >
---
+++ src/shared/constants/appLocales.ts
@@ -0,0 +1,1 @@
+export const appLocales = ['en', 'el'];
